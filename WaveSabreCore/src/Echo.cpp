07bc56bc0d8cb0f940181951329dbec246c0bd2c
Erik Faye-Lund <kusmabite@gmail.com>
2019-02-28 18:40:37 +0100

    Echo: make DelayBuffers normal members
    There's no good reason for keeping this as pointers, this only makes
    destructors hard to reason about.

--- WaveSabreCore/src/Echo.cpp
+++ WaveSabreCore/src/Echo.cpp
@@ -18,9 +18,6 @@ namespace WaveSabreCore
 		cross = 0.0f;
 		dryWet = .5f;
 
-		leftBuffer = new DelayBuffer();
-		rightBuffer = new DelayBuffer();
-
 		for (int i = 0; i < 2; i++)
 		{
 			lowCutFilter[i].SetType(StateVariableFilterType::Highpass);
@@ -30,8 +27,6 @@ namespace WaveSabreCore
 
 	Echo::~Echo()
 	{
-		delete leftBuffer;
-		delete rightBuffer;
 	}
 
 	void Echo::Run(double songPosition, float **inputs, float **outputs, int numSamples)
@@ -39,8 +34,8 @@ namespace WaveSabreCore
 		double delayScalar = 120.0 / (double)Helpers::CurrentTempo / 8.0 * 1000.0;
 		float leftBufferLengthMs = (float)((double)leftDelayCoarse * delayScalar + (double)leftDelayFine);
 		float rightBufferLengthMs = (float)((double)rightDelayCoarse * delayScalar + (double)rightDelayFine);
-		leftBuffer->SetLength(leftBufferLengthMs);
-		rightBuffer->SetLength(rightBufferLengthMs);
+		leftBuffer.SetLength(leftBufferLengthMs);
+		rightBuffer.SetLength(rightBufferLengthMs);
 
 		for (int i = 0; i < 2; i++)
 		{
@@ -53,11 +48,11 @@ namespace WaveSabreCore
 			float leftInput = inputs[0][i];
 			float rightInput = inputs[1][i];
 
-			float leftDelay = lowCutFilter[0].Next(highCutFilter[0].Next(leftBuffer->ReadSample()));
-			float rightDelay = lowCutFilter[1].Next(highCutFilter[1].Next(rightBuffer->ReadSample()));
+			float leftDelay = lowCutFilter[0].Next(highCutFilter[0].Next(leftBuffer.ReadSample()));
+			float rightDelay = lowCutFilter[1].Next(highCutFilter[1].Next(rightBuffer.ReadSample()));
 
-			leftBuffer->WriteSample(leftInput + (leftDelay * (1.0f - cross) + rightDelay * cross) * feedback);
-			rightBuffer->WriteSample(rightInput + (rightDelay * (1.0f - cross) + leftDelay * cross) * feedback);
+			leftBuffer.WriteSample(leftInput + (leftDelay * (1.0f - cross) + rightDelay * cross) * feedback);
+			rightBuffer.WriteSample(rightInput + (rightDelay * (1.0f - cross) + leftDelay * cross) * feedback);
 
 			outputs[0][i] = leftInput * (1.0f - dryWet) + leftDelay * dryWet;
 			outputs[1][i] = rightInput * (1.0f - dryWet) + rightDelay * dryWet;
