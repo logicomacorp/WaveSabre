8d58ddd700bf4cc2adbce8357eaa13da4dcbb024
Erik Faye-Lund <kusmabite@gmail.com>
2020-04-27 23:16:02 +0200

    use msvcrt.dll for x64 builds as well
    This allows us to use msvcrt.dll for x64-builds as well, stripping off a
    bunch of bytes from the binaries.

---
+++ MSVCRT/mainCRTStartup.c
@@ -0,0 +1,12 @@
+#if defined(_MSC_VER) && !defined(_M_X86)
+int __getmainargs(int *argc, char ***argv, char ***env, int dowildcards, int *new_mode);
+int main(int argc, char **argv);
+int mainCRTStartup()
+{
+	int argc;
+	char **argv, **env;
+	int new_mode = 0;
+	__getmainargs(&argc, &argv, &env, 0, &new_mode);
+	return main(argc, argv);
+}
+#endif
